eb0542fb215e412e046a71a8ca5d0a851806db52
sania7594 <47710445+sania7594@users.noreply.github.com>
2021-02-01 23:39:31 +0300

    freeMarker

--- src/main/java/com/example/personalaccount/controller/UserController.java
+++ src/main/java/com/example/personalaccount/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/user")
+//@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserRepository repository;
@@ -32,22 +33,43 @@ public class UserController {
 
     @GetMapping("/findone/{id}")
     public UserLK getOne(@PathVariable("id") UserLK userLK){return userLK;}
-    
-    @PostMapping("/create")
-    public UserLK create(@RequestBody UserLK userLK){
+
+
+    @GetMapping(value={"/user/create"})
+    public String init(@ModelAttribute("model") ModelMap model) {
+        model.addAttribute("repository", repository);
+        return "index";
+    }
+
+    public String showAddUser(Model model){
+        UserLK userLK=new UserLK();
+        model.addAttribute("create",true);
+        model.addAttribute("userLK",userLK);
+
+        return "user-edit";
+    }
+
+    @PostMapping(value={"/add"})
+    public UserLK addUser(Model model, @ModelAttribute("userLK") UserLK userLK){
         return repository.save(userLK);
     }
 
     //@PostMapping("/create")
-    //public UserLK create(@ModelAttribute("userLK") UserLK userLK){
+    //public UserLK create(@RequestBody UserLK userLK){
         //return repository.save(userLK);
     //}
 
-    //@PostMapping("/create")
-    //public UserLK create(@RequestBody UserLK userLK, Model model){
-        //model.addAttribute("userForm",new UserLK());
-        //return repository.save(userLK);
+    //@RequestMapping(value="/user", method = RequestMethod.GET)
+    //public String init(@ModelAttribute("userLK") ModelMap userLK){
+        //userLK.addAttribute("users",repository);
+       // return "OK";
     //}
+
+    @RequestMapping(value="/create", method = RequestMethod.POST)
+    public UserLK addUser(@ModelAttribute("userLK") UserLK userLK){
+        return repository.save(userLK);
+    }
+
 
     @PutMapping("/update/{id}")
     public UserLK update(@PathVariable("id") UserLK userUpdateDB, @RequestBody UserLK userLK){
