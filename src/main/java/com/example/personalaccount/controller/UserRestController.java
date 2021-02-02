e29852df7fcf7bb32329098a09ceb5e57992309e
sania7594 <47710445+sania7594@users.noreply.github.com>
2021-02-02 14:18:26 +0300

    add rest controll

--- src/main/java/com/example/personalaccount/controller/UserRestController.java
+++ src/main/java/com/example/personalaccount/controller/UserRestController.java
@@ -1,28 +1,21 @@
 package com.example.personalaccount.controller;
 
-
-import com.example.personalaccount.repository.*;
-import com.example.personalaccount.table.*;
+import com.example.personalaccount.repository.UserRepository;
+import com.example.personalaccount.table.UserLK;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
-@Controller
-//@RequestMapping("/user")
-public class UserController {
+@RestController
+public class UserRestController {
     @Autowired
     UserRepository repository;
 
-    public UserController(UserRepository repository){
+    public UserRestController(UserRepository repository){
         this.repository=repository;
     }
 
@@ -57,19 +50,19 @@ public class UserController {
 
     //@RequestMapping(value="add", method=RequestMethod.GET)
     //public String addUser(@ModelAttribute("userLK") UserLK userLK){
-        //repository.save(userLK);
-        //return "oK";
+    //repository.save(userLK);
+    //return "oK";
     //}
-   ///
+    ///
     //@PostMapping("/create")
     //public UserLK create(@RequestBody UserLK userLK){
-        //return repository.save(userLK);
+    //return repository.save(userLK);
     //}////
 
     //@RequestMapping(value="/user", method = RequestMethod.GET)
     //public String init(@ModelAttribute("userLK") ModelMap userLK){
-        //userLK.addAttribute("users",repository);
-       // return "OK";
+    //userLK.addAttribute("users",repository);
+    // return "OK";
     //}
 
     @RequestMapping(value="/add", method = RequestMethod.POST)
